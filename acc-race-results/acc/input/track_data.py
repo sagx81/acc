7b9c4c825b1ef67b629db6c2d68c3ceb8512a3ca
Kielkozaur <hardvan@gmail.com>
2024-09-02 20:08:35 +0200

    Add files via upload

---
+++ acc-race-results/acc/input/track_data.py
@@ -0,0 +1,115 @@
+# track_data.py
+track_data = {
+    'barcelona': {
+        "name": 'Circuit de Barcelona - Catalunya',
+        "year": 2018
+      },
+      'brands_hatch': {
+        "name": 'Brands Hatch Circuit',
+        "year": 2018
+      },
+      'cota': {
+      "name": 'Circuit of the Americas',
+      "year": 2022,
+      "dlc": 'American Track Pack'
+  },
+  'donington': {
+      "name": 'Donington Park',
+      "year": 2019,
+      "dlc": 'British GT Pack'
+  },
+  'hungaroring': {
+      "name": 'Hungaroring Circuit',
+      "year": 2018
+  },
+  'imola': {
+      "name": 'Autodromo Enzo e Dino Ferrari - Imola',
+      "year": 2020,
+      "dlc": '2020 GT World Challenge Pack'
+  },
+  'indianapolis': {
+      "name": 'Indianapolis Motor Speedway',
+      "year": 2022,
+      "dlc": 'American Track Pack'
+  },
+  'kyalami': {
+      "name": 'Kyalami Grand Prix Circuit',
+      "year": 2019,
+      "dlc": 'Intercontinental GT Pack'
+  },
+  'laguna_seca': {
+      "name": 'WeatherTech Raceway Laguna Seca',
+      "year": 2019,
+      "dlc": 'Intercontinental GT Pack'
+  },
+  'misano': {
+      "name": 'Misano',
+      "year": 2018
+  },
+  'monza': {
+      "name": 'Autodromo Nazionale Monza',
+      "year": 2018
+  },
+  'mount_panorama': {
+      "name": 'Bathurst Mount Panorama Circuit',
+      "year": 2019,
+      "dlc": 'Intercontinental GT Pack'
+  },
+  'nurburgring': {
+      "name": 'Nurburgring',
+      "year": 2018
+  },
+  'oulton_park': {
+      "name": 'Oulton Park',
+      "year": 2019,
+      "dlc": 'British GT Pack'
+  },
+  'paul_ricard': {
+      "name": 'Circuit - Paul Ricard',
+      "year": 2018
+  },
+  'red_bull_ring': {
+      "name": 'RedBull Ring',
+      "year": 2018
+  },
+  'silverstone': {
+      "name": 'Silverstone',
+      "year": 2018
+  },
+  'snetterton': {
+      "name": 'Snetterton Circuit',
+      "year": 2019,
+      "dlc": 'British GT Pack'
+  },
+  'spa': {
+      "name": 'Circuit de Spa-Francorchamps',
+      "year": 2018
+  },
+  'suzuka': {
+      "name": 'Suzuka Circuit',
+      "year": 2019,
+      "dlc": 'Intercontinental GT Pack'
+  },
+  'valencia': {
+      "name": 'Valencia - Circuit Ricardo Tormo',
+      "year": 2023,
+      "dlc": 'GT World Challenge 2023 Pack'
+  },
+  'watkins_glen': {
+      "name": 'Watkins Glen',
+      "year": 2022,
+      "dlc": 'American Track Pack'
+  },
+  'zandvoort': {
+      "name": 'Circuit Park Zandvoort',
+      "year": 2018
+  },
+  'nurburgring_24h': {
+      "name": 'nurburgring_24h',
+      "year": 2024
+  },
+  'zolder': {
+      "name": 'Circuit Zolder',
+      "year": 2018
+  }
+    }
