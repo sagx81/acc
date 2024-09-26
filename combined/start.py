0fe34810d827f19ec078adfc23471551c0eba1fb
sagx81 <31575085+sagx81@users.noreply.github.com>
2024-09-26 13:49:25 +0200

    cleaning

--- combined/start.py
+++ combined/start.py
@@ -1,9 +1,21 @@
-# import os
-import sys
+import os
+import shutil
+# import sys
 
 from ftp import ftp
 from process_results_phase1 import process_file_from_ftp
 from penalties_apply import penalties_apply
+
+current_dir = os.getcwd()
+#input_dir = os.path.join(current_dir, "fromFTP")
+results_phase1 = "process_results_phase1"
+processedFilesCSV = os.path.join(current_dir, results_phase1, "processed_files.csv") 
+
+#optional - CLEANING for testing purposes only
+if os.path.exists(processedFilesCSV):
+    os.remove(processedFilesCSV)
+shutil.rmtree("output_phase1")
+# os.rmdir("output_phase1")
 
 # adding to the system path
 # sys.path.insert(0, './results_phase1/')
