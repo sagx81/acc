c8a16fc5f471b4778e011bb684c3ba81f21145d5
pawel <sagx@wp.pl>
2024-12-27 23:11:15 +0100

    code cleaning

--- new/process_results_phase1/process_file_from_ftp copy.py
+++ new/process_results_phase1/process_file_from_ftp copy.py
@@ -14,6 +14,58 @@ from utils_entities.processed_files import ProcessedFile
 # from new.utils_entities.processed_files import ProcessedFile
 
 
+# from constants import points
+# from constants import dnf_text
+
+# from combined.constants import points
+# from PIL import Image, ImageDraw, ImageFont
+# from process_results_phase1.track_data import track_data
+# import track_data
+
+# def convert_time(ms):
+#     minutes = int(ms // 60000)
+#     seconds = int((ms % 60000) // 1000)
+#     milliseconds = int(ms % 1000)
+#     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
+
+# def generate_unique_filename(output_dir, base_name, extension="png"):
+#     counter = 1
+#     output_file = os.path.join(output_dir, f"{base_name}.{extension}")
+#     while os.path.exists(output_file):
+#         output_file = os.path.join(output_dir, f"{base_name}({counter}).{extension}")
+#         counter += 1
+#     return output_file
+
+# def find_project_root(start_path, target_dir_name="acc-race-results"):
+#     current_path = start_path
+#     while True:
+#         if os.path.basename(current_path) == target_dir_name:
+#             return current_path
+#         parent_path = os.path.dirname(current_path)
+#         if parent_path == current_path:
+#             raise FileNotFoundError(f"Katalog {target_dir_name} nie został znaleziony w ścieżce: {start_path}")
+#         current_path = parent_path
+
+# Ustalanie katalogu głównego projektu
+# project_root = find_project_root(os.path.abspath(__file__))
+
+# Definiowanie dynamicznych ścieżek do katalogów wejściowych, wyjściowych oraz do plików czcionki i tła
+# input_dir = os.path.join(project_root, "acc", "input")
+# output_dir = os.path.join(project_root, "acc", "output", "GT3 S2")
+# background_image = os.path.join(project_root, "files", "background", "race results.png")
+# font_path = os.path.join(project_root, "files", "fonts", "BigShouldersDisplay-Bold.ttf")
+
+
+# points = [20, 16, 13, 11, 9, 7, 5, 4, 3, 2, 1, 1, 1, 1, 1]
+# points = points.get_points_table()
+# points = constants.points
+
+# GRAFIKA
+# DNF_TEXT = "DNF"
+# DNF_COLOR = "red"
+# DEFAULT_COLOR = "#191919"
+# MAGENTA_COLOR = "magenta"
+
 current_dir = os.getcwd()
 input_dir = constants.files_from_ftp
 output_phase1 = constants.files_result_phase_1
@@ -88,6 +140,9 @@ def get_race_results():
                 #print(f"\n Output dir: {output_dir}")
 
                 short_name = data.get('trackName', 'unknown_track')
+                # track_name = constants.get_track_name(short_name)
+                # track_info = track_data.get(short_name, {})
+                # track_name = track_info.get('name', short_name)
 
                 base_name = os.path.basename(json_file)
                 date_part = base_name.split('_')[0]
@@ -138,7 +193,8 @@ def get_race_results():
                         total_time_str = constants.dnf_text
                         best_lap_str = constants.dnf_text
                         points_awarded = 0
-                    # elif isQuali: ?????
+                    # elif isQuali:
+
                     else:
                         if i == 0:
                             first_driver_time = total_time_ms
@@ -215,6 +271,63 @@ def get_race_results():
                 # processedFiles.append({os.path.basename(json_file), datetime.datetime.now()})
                 # processedFiles.append(ProcessedFile(os.path.basename(json_file), datetime.datetime.now()) )
 
+
+            # GRAFIKA
+
+            # if not os.path.isfile(background_image):
+            #     raise FileNotFoundError(f"Background image not found: {background_image}")
+
+            # bg_image = Image.open(background_image)
+            # draw = ImageDraw.Draw(bg_image)
+
+            # font_size = 47
+            # try:
+            #     font = ImageFont.truetype(font_path, font_size)
+            # except IOError:
+            #     print(f"Could not load font at {font_path}. Using default font.")
+            #     font = ImageFont.load_default()
+
+            # header_x_start = 20
+            # header_y_start = 145
+            # header_line_height = 40
+
+            # column_widths = [170, 500, 400, 400, 250, 250]
+
+            # for j, header in enumerate(results[0]):
+            #     x_position = header_x_start + sum(column_widths[:j]) + column_widths[j] // 2
+            #     text_bbox = draw.textbbox((0, 0), header, font=font)
+            #     text_width = text_bbox[2] - text_bbox[0]
+            #     draw.text((x_position - text_width // 2, header_y_start), str(header), fill="grey", font=font)
+
+            # title_text = track_name
+            # title_bbox = draw.textbbox((0, 0), title_text, font=font)
+            # title_width = title_bbox[2] - title_bbox[0]
+            # title_x = (bg_image.width - title_width) // 2
+            # title_y = header_y_start - header_line_height - 60
+
+            # draw.text((title_x, title_y), title_text, fill="black", font=font)
+
+            # result_x_start = 20
+            # result_y_start = header_y_start + header_line_height + 38
+            # result_line_height = 96
+
+            # for i, row in enumerate(results[1:], start=1):
+            #     y_position = result_y_start + (i - 1) * result_line_height
+            #     for j, cell in enumerate(row):
+            #         x_position = result_x_start + sum(column_widths[:j]) + column_widths[j] // 2
+            #         text_bbox = draw.textbbox((0, 0), str(cell), font=font)
+            #         text_width = text_bbox[2] - text_bbox[0]
+            #         if cell == DNF_TEXT:
+            #             draw.text((x_position - text_width // 2, y_position), str(cell), fill=DNF_COLOR, font=font)
+            #         elif j == 3 and cell == fastest_lap_time_converted:
+            #             draw.text((x_position - text_width // 2, y_position), str(cell), fill=MAGENTA_COLOR, font=font)
+            #         else:
+            #             draw.text((x_position - text_width // 2, y_position), str(cell), fill=DEFAULT_COLOR, font=font)
+
+            # bg_image.save(output_image_file)
+            # print(f"Ranking został zapisany do pliku: {output_image_file}")
+
+            # os.remove(json_file)
 
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON from file {json_file}: {e}")
